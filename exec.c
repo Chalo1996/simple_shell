fc198b412effdbee278f2a56df055268b364ccb1
Chalo1996 <emusyoka759@gmail.com>
2022-06-16 22:23:52 +0000

    Execute all commands in entered

--- exec.c
+++ exec.c
@@ -4,25 +4,25 @@
   *ext- Verify and execute every command
   *@commands: command entered
   *@buf: commands buffer.
-  *@envp: enviroment variables
-  *@av: argument counter
+  *@env: enviroment variables
+  *@argv: argument counter
   *@counter: execution counter.
   *
   *Return: (void)
   */
 
-void ext(char **commands, char *buf, char **envp, char **av, int counter)
+void ext(char **commands, char *buf, char **env, char **argv, int counter)
 {
 	struct stat f1Stat;
 
 	if (commands == NULL)
-		free(buf);
+		freebuf(buf);
 	else if (_strcmp("exit", commands[0]))
 		prnt(buf, commands);
 	else if (_strcmp("env", commands[0]))
-		freebufend(buf, commands, envp);
+		freebufend(buf, commands, env);
 	else if (stat(commands[0], &f1Stat) == 0)
 		execve(commands[0], commands, NULL);
 	else
-		_path(commands, buf, envp, av, counter);
+		_path(commands, buf, env, argv, counter);
 }
